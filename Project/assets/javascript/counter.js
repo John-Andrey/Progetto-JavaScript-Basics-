37be7ad0d8f67aee99174f2833b56181bc4f9bb3
John-Andrey <76631255+John-Andrey@users.noreply.github.com>
2021-11-03 23:37:28 +0100

    Add files via upload

---
+++ Project/assets/javascript/counter.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+var count = 0;
+
+function add() {
+    count += 1;
+    document.getElementById('counter').innerHTML = count;
+}
+
+function lower() {
+    count -= 1;
+    document.getElementById('counter').innerHTML = count;
+}
